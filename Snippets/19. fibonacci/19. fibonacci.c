9ca38fbd52d2aac7a8b500e04e0bc8ef701e32c0
ismqdb <djukic.djukic96@gmail.com>
2024-10-18 08:59:08 +0200

    y

--- Snippets/19. fibonacci/19. fibonacci.c
+++ Snippets/19. fibonacci/19. fibonacci.c
@@ -1,6 +1,9 @@
 #include "./19. fibonacci.h"
 
 int fibonacci_recursive(int n){
+    if(n < 0)
+        return -1;
+
     if(n <= 1)
         return 1;
     
@@ -9,11 +12,11 @@ int fibonacci_recursive(int n){
 
 int fibonacci_iterative(int n){
     int i;
-    int array[n];
+    int array[n+2];
     array[0] = array[1] = 1;
 
     for(i = 2; i <= n; i++)
-        array[i] = array[i-1] + array[n-2];
+        array[i] = array[i-1] + array[i-2];
         
     return array[n];
 }
