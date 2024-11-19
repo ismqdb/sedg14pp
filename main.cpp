4b437eeb6374d282542f881d481e8f25b39c11b6
ismqdb <djukic.djukic96@gmail.com>
2024-11-19 20:08:44 +0100

    Tree node RS

--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@
 #include <stack>
 #include <queue>
 
+#include "Snippets/00. includes/tree node RS/treeNodeRS.hpp"
+
 /* ******************************************************************************** */
 
 // A B C + D E * * F + *
 
 int main(){
-    
+    treeNodeRS<char> *node;
+
+    node->info = 'a';
+    node->left = NULL;
+    node->right = NULL;
+    node->parent = NULL;
 }
 
 /* ******************************************************************************** */
