4454b61865d1f31b66320e777bd301e43e4ad155
ismqdb <djukic.djukic96@gmail.com>
2024-11-28 17:47:18 +0100

    Setup

--- main.c
+++ main.c
@@ -3,9 +3,8 @@
 #include <string.h>
 #include <math.h>
 
-#include "Snippets/19. fibonacci/19. fibonacci.h"
+
 
 int main(){
-    printf("%d\n", fibonacci_rr1(10));
-    printf("%d\n", fibonacci_rr2(10));
+
 }
