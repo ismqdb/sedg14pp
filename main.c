37cc42d5b345d7cc377b7bd1c10030e48bc50cc2
ismqdb <djukic.djukic96@gmail.com>
2024-11-28 17:28:19 +0100

    y

--- main.c
+++ main.c
@@ -3,11 +3,6 @@
 #include <string.h>
 #include <math.h>
 
-#include "Snippets/00. includes/tree node/tree_node.h"
-
 int main(){
-    draw_binary_tree_recursive_driver();
     
 }
-
-// A B C + D E * * F + *
