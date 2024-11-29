abcf9c1ec39871798a1a0392fb621623a2d0ff9f
ismqdb <djukic.djukic96@gmail.com>
2024-11-29 21:07:28 +0100

    y

--- Snippets/22. recursion removal/22. recursion_removal_inorder.c
+++ Snippets/22. recursion removal/22. recursion_removal_inorder.c
@@ -3,3 +3,30 @@
 #include "./22. recursion_removal_inorder.h"
 
 /* ******************************************************************************** */
+
+void in_order_rr0(tree_node *t){
+    stack_a stack = stack_a_init(TREE_NODE, 50);
+    tree_node *temp;
+    
+    do {
+        if(t->left != NULL){
+            temp = t->left;
+            t->left = NULL;
+            stack_a_push_tree_node(&stack, t);
+            t = temp;
+        } else {
+            tree_node_visit(t);
+            t = stack_a_pop_tree_node(&stack);
+            stack_a_pop_tree_node(&stack);
+            
+            if(t->right != NULL){
+                stack_a_push_tree_node(&stack, t->right);
+                t->right = NULL;
+            }
+        }
+    } while(stack_a_is_empty(&stack));
+
+    stack_a_deinit(&stack);
+}
+
+/* ******************************************************************************** */
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <math.h>
 
-
+#include "Snippets/22. recursion removal/22. recursion_removal_inorder.h"
 
 int main(){
 
