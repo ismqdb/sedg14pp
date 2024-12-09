da2b1764a8513ab860009b011a8dffe79ff4d862
ismqdb <djukic.djukic96@gmail.com>
2024-12-10 00:05:31 +0100

    post order RR setup

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 #include <math.h>
 
 #include "./Snippets/00. includes/parse tree/parseTree.h"
-#include "./Snippets/22. recursion removal/22. recursionRemovalInOrder.h"
+#include "./Snippets/22. recursion removal/22. recursionRemovalPostOrder.h"
 
 int main(){
     struct treeNode *t = buildParseTree();
 
-    inOrderRR0(t);
+    postOrderRR0(t);
     putchar(10);
 
-    inOrderRR1(t);
+    postOrderRR1(t);
     putchar(10);
 }
 
