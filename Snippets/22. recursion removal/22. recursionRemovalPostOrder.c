a65c2b41ce67c72c4f27ec8505677a951158992b
ismqdb <djukic.djukic96@gmail.com>
2024-12-10 00:47:39 +0100

    post order works?

--- Snippets/22. recursion removal/22. recursionRemovalPostOrder.c
+++ Snippets/22. recursion removal/22. recursionRemovalPostOrder.c
@@ -15,7 +15,39 @@ void postOrderRR0(struct treeNode *t){
 /* ******************************************************************************** */
 
 void postOrderRR1(struct treeNode *t){
+    struct stackArray stack = stackArrayInit(INT, 50);
+    struct treeNode *temp;
 
+    l:
+        if(t == NULL)
+            goto r;
+
+        stackArrayPushTreeNode(&stack, t);
+        t = t->left;
+        goto l;
+
+    r:
+        t = stackArrayPopTreeNode(&stack);
+
+        if(t->right == NULL){
+            if(stackArrayIsEmpty(&stack)){
+                visitTreeNode(t);
+                goto x;
+            }
+        }
+        else {
+            stackArrayPushTreeNode(&stack, t);
+            temp = t->right;
+            t->right = NULL;
+            t = temp;
+            goto l;
+        }
+
+        visitTreeNode(t);
+        goto r;
+
+    x:
+        return;
 }
 
 /* ******************************************************************************** */
