ae1b8978f3d06df4c052c4aecffdadd3a0dbdc9c
ismqdb <djukic.djukic96@gmail.com>
2024-12-19 20:52:36 +0100

    y

--- main.c
+++ main.c
@@ -6,16 +6,10 @@
 #include <math.h>
 #include <assert.h>
 
-#include "./Snippets/01. gcd/01. gcd.h"
-
 /* ******************************************************************************** */
 
 int main(){
-    unsigned long long x = 200;
-    unsigned long long y = 175;
 
-    printf("%d\n", gcd(x, y));
-    printf("%d\n", gcdRecursive(&x, &y));
 }
 
 /* ******************************************************************************** */
