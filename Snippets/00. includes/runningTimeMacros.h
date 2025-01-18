d9a0c29cf765f2a4bfa70097fece7680be26cb51
ismqdb <djukic.djukic96@gmail.com>
2025-01-18 23:08:19 +0100

    Running time macro

---
+++ Snippets/00. includes/runningTimeMacros.h
@@ -0,0 +1,15 @@
+/* ******************************************************************************** */
+
+#define runningTime(function, result) do { \
+    clock_t t; \
+    double sec;\
+    \
+    t = clock(); \
+    \
+    function; \
+    sec = ((double)(clock() - t)) / CLOCKS_PER_SEC; \
+    result = sec; \
+    } \
+    while(0)
+
+/* ******************************************************************************** */
