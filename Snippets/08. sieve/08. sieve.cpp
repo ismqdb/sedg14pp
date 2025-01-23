ba6e9a864b87268a756ed0d33ea6b9a5a5694ef0
ismqdb <djukic.djukic96@gmail.com>
2025-01-23 18:25:45 +0100

    Set up to test sieve

--- Snippets/08. sieve/08. sieve.cpp
+++ Snippets/08. sieve/08. sieve.cpp
@@ -3,16 +3,17 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <assert.h>
+#include <limits>
+#include <chrono>
 
 /* ******************************************************************************** */
 
-void sieve(int size){
-    if(size < 2){
-        fprintf(stderr, "Sieve: less than 2 elements.\n");
-        return;
-    }
+int sieve(int size){
+    assert(size>2);
 
-    int i,j;
+    int i;
+    int j;
     
     std::vector<int> vec(size);
 
@@ -24,11 +25,47 @@ void sieve(int size){
             if((i*j)<size)
                 vec[i*j] = 0;
 
+    int biggestIdx = 0;
+
     for(i = 1; i < size; i++)
         if(vec[i])
-            std::cout << i << ' ';
+            biggestIdx = i;
 
-    putchar(10);
+    return biggestIdx;
+}
+
+/* ******************************************************************************** */
+
+int sieveUnbounded(){
+    unsigned long long i;
+    unsigned long long j;
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    size_t size = //std::numeric_limits<unsigned long long>::max();
+    30000000;
+
+    std::vector<int> vec(pow(10, 9));
+
+    for(vec[1] = 0, i = 2; i < size; i++)
+        vec[i] = 1;
+
+    for(i = 2; i <= size / 2; i++)
+        for(j = 2; j <= size / i; j++)
+            if((i * j) < size)
+                vec[i * j] = 0;
+
+    int biggestIdx = 0;
+
+    for(i = 1; i < size; i++)
+        if(vec[i])
+            biggestIdx = i;
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000000.0 << " seconds";
+
+    return biggestIdx;
 }
 
 /* ******************************************************************************** */
