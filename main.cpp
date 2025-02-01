b14ce4e4de89f3a8b9c097fe8f7efebe4aceea0e
ismqdb <djukic.djukic96@gmail.com>
2025-02-01 05:56:51 +0100

    Ch 06 Ex 07

--- main.cpp
+++ main.cpp
@@ -16,20 +16,7 @@
 int main(){
     treeNode<char> *tree = buildParseTree<char>();
 
-    double x1;
-    double x2;
-    double x3;
-
-    runningTime(preorderIterative<char>(tree), x1);
-    putchar(10);
-    runningTime(preorderRecursive<char>(tree), x2);
-    putchar(10);
-    runningTime(preOrderRR4(tree), x3);
-    putchar(10);
-
-    std::cout << "Pre order iterative:\t\t" << x1 * pow(10, 9) << " n seconds.\n";
-    std::cout << "Pre order recursive:\t\t" << x2 * pow(10, 9) << " n seconds.\n";
-    std::cout << "Pre order RR inline:\t\t" << x3 * pow(10, 9) << " n seconds.\n";
+    preorderRecursive(tree);
 }
 
 /* ******************************************************************************** */
