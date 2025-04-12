8fe7b8fd4146df92416505ba01034cf2958d70a8
ismqdb <djukic.djukic96@gmail.com>
2025-04-12 21:39:55 +0200

    Better naming

--- Snippets/23. sort/23. sort.cpp
+++ Snippets/23. sort/23. sort.cpp
@@ -4,70 +4,70 @@
 
 /* ******************************************************************************** */
 
-void selectionSort(i32 a[], i32 n){
+void selectionSort(i32 array[], i32 size){
     i32 min;
     i32 temp;
 
-    for(i32 i = 0; i < n; i++){
+    for(i32 i = 0; i < size; i++){
         min = i;
 
-        for(i32 k = i; k < n; k++)
-            if(a[k] < a[min])
+        for(i32 k = i; k < size; k++)
+            if(array[k] < array[min])
                 min = k;
 
-        temp = a[i];
-        a[i] = a[min];
-        a[min] = temp;
+        temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
     }
 }
 
 /* ******************************************************************************** */
 
-void insertionSort(i32 a[], i32 n){
-    for(i32 i = 1; i <= n; i++){
+void insertionSort(i32 array[], i32 size){
+    for(i32 i = 1; i <= size; i++){
         i32 k = i;
-        i32 temp = a[k];
+        i32 temp = array[k];
 
-        while(temp < a[k-1]){
-            a[k] = a[k-1];
+        while(temp < array[k-1]){
+            array[k] = array[k-1];
             k--;
         }
-        a[k] = temp;
+        array[k] = temp;
     }
 }
 
 /* ******************************************************************************** */
 
-void bubbleSort(i32 a[], i32 n){
-    i32 t;
+void bubbleSort(i32 array[], i32 size){
+    i32 temp;
 
-    for(i32 i = n; i >= 1; i--)
+    for(i32 i = size; i >= 1; i--)
         for(i32 j = 0; j <= i; j++)
-            if(a[j-1] > a[j]){
-                t = a[j-1];
-                a[j-1] = a[j];
-                a[j] = t;
+            if(array[j-1] > array[j]){
+                temp = array[j-1];
+                array[j-1] = array[j];
+                array[j] = temp;
             }
 }
 
 /* ******************************************************************************** */
 
-void insertionSortLargeRecord(i32 a[], i32 p[], i32 n){
+void insertionSortLargeRecord(i32 array[], i32 indexes[], i32 size){
     i32 v;
     i32 j;
 
-    for(i32 i = 0; i <= n; i++)
-        p[i] = i;
+    for(i32 i = 0; i <= size; i++)
+        indexes[i] = i;
 
-    for(i32 i = 2; i <= n; i++){
-        v = p[i];
+    for(i32 i = 2; i <= size; i++){
+        v = indexes[i];
         j = i;
 
-        while(a[p[j-1]] > a[v]){
-            p[j] = p[j-1];
+        while(array[indexes[j-1]] > array[v]){
+            indexes[j] = indexes[j-1];
             j--;
         }
-        p[j] = v;
+        indexes[j] = v;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -14,15 +14,6 @@
 
 int main(){
     std::vector<i32> vec = getRandVec(10, 0, 50);
-    selectionSort(vec.data(), vec.size());
-
-    vec = getRandVec(10, 0, 50);
-    insertionSort(vec.data(), vec.size()); 
-
-    vec = getRandVec(10, 0, 50);
-    bubbleSort(vec.data(), vec.size()); 
-
-    vec = getRandVec(10, 0, 50);
     i32 sortHelp[10];
     insertionSortLargeRecord(vec.data(), sortHelp, vec.size());
 
