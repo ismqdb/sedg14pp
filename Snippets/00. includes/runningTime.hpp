b08e0b9ba818994d055c0f43a90b4dbbbe2cbb02
ismqdb <djukic.djukic96@gmail.com>
2025-09-30 21:34:59 +0200

    milli

--- Snippets/00. includes/runningTime.hpp
+++ Snippets/00. includes/runningTime.hpp
@@ -1,10 +1,10 @@
 #include <chrono>
 
-#define runningTimeNanoSeconds(function, result) do { \
+#define runningTimeMilliSeconds(function, result) do { \
     auto start = std::chrono::steady_clock::now(); \
     function; \
     auto end = std::chrono::steady_clock::now(); \
     auto diff = end - start; \
-    result = std::chrono::duration<double, std::nano>(diff).count(); \
+    result = std::chrono::duration<double, std::milli>(diff).count(); \
     } \
 while(0)
