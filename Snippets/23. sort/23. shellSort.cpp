821fb269db395d69f9e3039c06b5a9744e2ee35e
ismqdb <djukic.djukic96@gmail.com>
2025-10-23 13:48:50 +0200

    increment sequences

--- Snippets/23. sort/23. shellSort.cpp
+++ Snippets/23. sort/23. shellSort.cpp
@@ -27,24 +27,84 @@ none shellSort(i8 array[], i32 size) {
 
 /* ******************************************************************************** */
 
-i32 shell_inc(i32 size) {
+std::vector<i32> shell_seq(i32 size) {
+    std::vector<i32> vec;
     i32 h;
 
     for (h = 1; h <= size / 9; h = 3 * h + 1)
-        ;
+        vec.push_back(h);
 
-    return h;
+    return vec;
 }
 
 /* ******************************************************************************** */
 
-i32 shell_dec(i32 size) {
-    i32 coef = 3;
+std::vector<i32> knuth_seq(i32 size) {
+    std::vector<i32> vec;
+    i32 h = 1;
 
-    if (size > 1 && (size / coef) == 0)
-        return 1;
-    else
-        return size / coef;
+    for (i32 i = 0; h <= size / 9; i++) {
+        vec.push_back(h);
+        h = pow(3, i) / 2;
+    }
+
+    return vec;
+}
+
+/* ******************************************************************************** */
+
+std::vector<i32> hibbard_seq(i32 size) {
+    std::vector<i32> vec;
+    i32 h = 1;
+
+    for (i32 i = 0; h <= size / 9; i++) {
+        vec.push_back(h);
+        h = pow(2, i) - 1;
+    }
+
+    return vec;
+}
+
+/* ******************************************************************************** */
+
+std::vector<i32> sedg_seq_1(i32 size) {
+    std::vector<i32> vec;
+    i32 h = 1;
+
+    for (i32 i = 0; h <= size / 9; i++) {
+        vec.push_back(h);
+        h = 9 * pow(2, i) - 9 * pow(2, i / 2) + 1;
+    }
+
+    return vec;
+}
+
+/* ******************************************************************************** */
+
+std::vector<i32> sedg_seq_2(i32 size) {
+    std::vector<i32> vec;
+    i32 h = 1;
+
+    for (i32 i = 0; h <= size / 9; i++) {
+        vec.push_back(h);
+        h = pow(4, i) - 3 * pow(2, i) + 1;
+    }
+
+    return vec;
+}
+
+/* ******************************************************************************** */
+
+std::vector<i32> tokuda_seq(i32 size) {
+    std::vector<i32> vec;
+    i32 h = 1;
+
+    for (i32 i = 0; h <= size / 9; i++) {
+        vec.push_back(h);
+        h = ceil(2.25 * h);
+    }
+
+    return vec;
 }
 
 /* ******************************************************************************** */
