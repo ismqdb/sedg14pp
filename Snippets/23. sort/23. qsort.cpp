0ef2446b590605e352c56df5ef32e6b8287acdd2
ismqdb <djukic.djukic96@gmail.com>
2025-11-21 09:07:26 +0100

    partition

--- Snippets/23. sort/23. qsort.cpp
+++ Snippets/23. sort/23. qsort.cpp
@@ -4,25 +4,25 @@
 
 /* ******************************************************************************** */
 
-none qsort(i32 array[], i32 left, i32 right){
+i32 partition(i32 array[], i32 left, i32 right) {
     i32 i;
     i32 pivot;
     i32 j;
     i32 temp;
 
-    if(right > left){
+    if (right > left) {
         pivot = array[right];
-        i = left-1;
+        i = left - 1;
         j = right;
 
-        while(1){
-            while(array[++i] < pivot)
-                ;
-            
-            while(array[--j] > pivot)
+        while (1) {
+            while (array[++i] < pivot)
                 ;
 
-            if(i>=j) break;
+            while (array[--j] > pivot)
+                ;
+
+            if (i >= j) break;
 
             temp = array[i];
             array[i] = array[j];
@@ -32,7 +32,18 @@ none qsort(i32 array[], i32 left, i32 right){
         temp = array[i];
         array[i] = array[right];
         array[right] = temp;
+    }
 
+    return i;
+}
+
+/* ******************************************************************************** */
+
+none qsort(i32 array[], i32 left, i32 right){
+    i32 i;
+
+    if(right > left){
+        i = partition(array, left, right);
         qsort(array, left, i-1);
         qsort(array, i+1, right);
     }
